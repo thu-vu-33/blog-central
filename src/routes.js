e487dbb9bde97cc17de63890242529fe38591b8a
Tittoh <tittohb@gmail.com>
2020-02-03 06:04:23 +0300

    Add create and read articles

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Switch, Route, Redirect,
+} from 'react-router-dom';
 
+import Create from './containers/Article/Create';
+import NotFound from './components/NotFound';
+import Read from './containers/Article/Read';
+import getCurrentUser from './utils/auth';
 import configStore from './store';
-import SocialAuth from './containers/Login';
 import Signin from './containers/Signin'
 import SignUp from './containers/SignUp';
 import ForgotPasssword from './containers/ForgotPassword';
@@ -13,17 +18,32 @@ import Home from './containers/Home';
 import ROUTES from './utils/routes';
 
 const store = configStore();
+const user = getCurrentUser();
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (user ? (
+      <Component {...props} user={user} />
+    ) : (
+      <Redirect to={{ pathname: ROUTES.signin, state: { from: props.location } }} />
+    ))
+    }
+  />
+);
 
 export default () => (
   <Provider store={store}>
     <Router>
       <Switch>
       <Route exact path={ROUTES.home} component={Home} />
-        <Route exact path={ROUTES.signin} component={Signin} />
-        <Route path={ROUTES.signup} component={SignUp} />
-        <Route exact path={ROUTES.login} component={SocialAuth} />
         <Route exact path={ROUTES.resetPassword} component={ForgotPasssword} />
         <Route exact path={ROUTES.verify} component={ResetPasssword} />
+        <Route path={ROUTES.getArticleUrl} component={Read} />
+        <PrivateRoute exact path={ROUTES.createArticleUrl} component={Create} />
+        <Route exact path={ROUTES.signup} component={SignUp} />
+        <Route exact path={ROUTES.signinWithEmail} component={Signin} />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   </Provider>
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -4,6 +4,9 @@ const ROUTES = {
     signin: '/signin',
     resetPassword: '/reset/password',
     signup: '/signup',
+    getArticleUrl: '/article/:id',
+    createArticleUrl: '/articles/new',
+    signinWithEmail: '/signin/email',
     verify: '/verify/:token',
   };
   
